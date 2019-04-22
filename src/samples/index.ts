cf57c787b60ee7deee7e8ad97ab8d23d7e3a0d8d
Niklas Mollenhauer <nikeee@outlook.com>
2019-04-23 00:29:07 +0200

    Fix breaking change in raw-loader 2.0.0

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,13 +1,25 @@
-export const tutorial = require("../samples/tutorial.gv");
+import Tutorial from "./tutorial.gv";
+import StatemMachine from "./state-machine.gv";
+import Clustering0 from "./clustering-0.gv";
+import Clustering1 from "./clustering-1.gv";
+import HuffmanTree from "./huffman-tree.gv";
+import HammingDistance from "./hamming-distance.gv";
+import AttributeDemo from "./attribute-distance.gv";
+import Contact from "./contact.gv";
+import Empty from "./empty-graph.gv";
 
 export const samples = {
-	"State Machine": require("../samples/state-machine.gv"),
-	"Clustering": require("../samples/clustering-0.gv"),
-	"Clustering 2": require("../samples/clustering-1.gv"),
-	"Huffman Tree": require("../samples/huffman-tree.gv"),
-	"Hamming Distance": require("../samples/hamming-distance.gv"),
-	"Attribute Demo": require("../samples/attribute-distance.gv"),
-	"Contact": require("../samples/contact.gv"),
-	"Tutorial": tutorial,
-	"Empty Graph": require("../samples/empty-graph.gv"),
+	"State Machine": StatemMachine,
+	"Clustering": Clustering0,
+	"Clustering 2": Clustering1,
+	"Huffman Tree": HuffmanTree,
+	"Hamming Distance": HammingDistance,
+	"Attribute Demo": AttributeDemo,
+	"Contact": Contact,
+	"Tutorial": Tutorial,
+	"Empty Graph": Empty,
+};
+
+export {
+	Tutorial as tutorial,
 };
