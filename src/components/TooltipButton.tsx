7eb18a2199a93752727cd3c7ca45a397ad77b4a5
Niklas Mollenhauer <nikeee@outlook.com>
2025-05-13 22:21:26 +0200

    Use newer components

--- src/components/TooltipButton.tsx
+++ src/components/TooltipButton.tsx
@@ -1,15 +1,14 @@
-import * as React from "react";
+import { Component, type PropsWithChildren, createRef } from "react";
 
-interface Props {
+interface Props extends PropsWithChildren {
 	onClick(): boolean;
 	title?: string;
 
 	className?: string;
-	children: any
 }
 
-export class TooltipButton extends React.Component<Props, object> {
-	private buttonRef: React.RefObject<HTMLButtonElement> = React.createRef();
+export class TooltipButton extends Component<Props, object> {
+	private buttonRef: React.RefObject<HTMLButtonElement> = createRef();
 
 	private timeout: ReturnType<typeof setTimeout> | undefined;
 
