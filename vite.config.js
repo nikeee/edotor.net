18de3fd7957ae395ff7e5a6f30d9f98d1494b97f
Niklas Mollenhauer <nikeee@outlook.com>
2025-05-13 21:07:26 +0200

    Properly use env vars

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
 import * as path from 'node:path';
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-	resolve: {
-		alias: {
-			"~": path.resolve(__dirname, './node_modules'),
-		}
-	},
-	plugins: [
-		ViteEjsPlugin({
-			// TODO: Add CI logic
-			includeMatomo: !!import.meta.env.VITE_MATOMO_API_BASE,
-			matomoApiBase: import.meta.env.VITE_MATOMO_API_BASE,
-		}),
-		react(),
-	],
-});
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		resolve: {
+			alias: {
+				"~": path.resolve(__dirname, './node_modules'),
+			}
+		},
+		plugins: [
+			ViteEjsPlugin({
+				// TODO: Add env vars to CI
+				includeMatomo: !!env.VITE_MATOMO_API_BASE,
+				matomoApiBase: env.VITE_MATOMO_API_BASE,
+			}),
+			react(),
+		],
+	}
+}
+);
