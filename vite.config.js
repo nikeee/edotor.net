a5d3765ba07fede00de4073a131eb22362dd3a47
Niklas Mollenhauer <nikeee@outlook.com>
2025-05-13 15:36:30 +0200

    Fix scss imports

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import * as path from 'node:path';
+
 import { defineConfig } from 'vite'
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, './node_modules'),
+		}
+	},
 	plugins: [
 		ViteEjsPlugin({
 			// TODO: Add CI logic
