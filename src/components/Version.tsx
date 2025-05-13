f5d6fdbf6ae6fec62343447456b8f34d856a7dbe
Niklas Mollenhauer <nikeee@outlook.com>
2025-05-13 21:00:18 +0200

    Use vite env var

--- src/components/Version.tsx
+++ src/components/Version.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+const version = import.meta.env.VITE_VERSION;
+
 export const Version = (props: any) => (
-	<span {...props} title={`Version ${VERSION}`}>v{VERSION}</span>
+	<span {...props} title={`Version ${version}`}>v{version}</span>
 );
