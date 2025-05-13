200918426730681275a98e78a0c8a85e9690c575
Niklas Mollenhauer <nikeee@outlook.com>
2025-05-14 01:17:55 +0200

    Fix types

--- src/components/SplitEditor.tsx
+++ src/components/SplitEditor.tsx
@@ -93,6 +93,8 @@ export default class SplitEditor extends React.Component<Props, State> {
 				minSize={50}
 				defaultSize={getSplitConfig() || "50%"}
 				onChange={size => saveSplitConfig(size)}
+				// biome-ignore lint/suspicious/noExplicitAny: hack for: https://github.com/tomkp/react-split-pane/issues/830#issuecomment-2788356773
+				{...{} as any}
 			>
 				<ErrorBoundary fallback="Could not load editor">
 					<EditorPane
