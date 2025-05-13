0743d9badb06370dfa8381ba1b190d69fb122078
Niklas Mollenhauer <nikeee@outlook.com>
2025-05-13 15:28:32 +0200

    Newer target

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [
 		ViteEjsPlugin({
+			// TODO: Add CI logic
 			includeMatomo: false,
 			matomoApiBase: '',
 		}),
