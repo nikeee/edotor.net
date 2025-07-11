0b4f71f8d100085332bd685d5f856c7447e3ee01
Niklas Mollenhauer <nikeee@outlook.com>
2025-07-11 19:49:33 +0200

    Import bootstrap styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { Component, createRef, lazy, type RefObject, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 
 import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.scss";
 
