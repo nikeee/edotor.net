aaa7cacc1cfdef0255370b3b328a998090df4e0c
Niklas Mollenhauer <nikeee@outlook.com>
2025-10-28 13:25:20 +0100

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,13 @@
 import parser from "@typescript-eslint/parser";
 import { defineConfig } from "eslint/config";
-import * as reactHooks from "eslint-plugin-react-hooks";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default defineConfig([
 	{
 		files: ["*/**/*.{js,ts,tsx,jsx}"],
 		languageOptions: { parser, sourceType: "module" },
-		rules: {
-			"react-hooks/react-compiler": "error",
-		},
+		...reactHooks.configs.flat.recommended,
+	}, {
+		ignores: ["dist/", "node_modules/"],
 	},
-	reactHooks.configs.recommended,
 ]);
