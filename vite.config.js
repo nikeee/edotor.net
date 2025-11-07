c730866adad42dc3956de1d89cf69fb336a0f20b
Niklas Mollenhauer <nikeee@outlook.com>
2025-11-07 21:31:20 +0100

    Format

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import * as path from 'node:path';
+import * as path from "node:path";
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from "vite"
 import { ViteEjsPlugin } from "vite-plugin-ejs";
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from "vite-plugin-static-copy"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd());
 	return {
 		resolve: {
 			alias: {
-				"~": path.resolve(__dirname, './node_modules'),
+				"~": path.resolve(__dirname, "./node_modules"),
 				"vscode": path.resolve(__dirname, "./node_modules/monaco-languageclient/lib/vscode-compatibility"),
 			}
 		},
@@ -18,8 +18,8 @@ export default defineConfig(({ mode }) => {
 			viteStaticCopy({
 				targets: [
 					{
-						src: 'CNAME',
-						dest: ''
+						src: "CNAME",
+						dest: "",
 					}
 				]
 			}),
