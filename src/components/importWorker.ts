b4f310f446ecb2e73c0284676cd4b7781dc2fd5f
Niklas Mollenhauer <nikeee@outlook.com>
2025-12-06 03:57:16 +0100

    Remove unused workers

--- src/components/importWorker.ts
+++ src/components/importWorker.ts
@@ -1,9 +1,9 @@
 import * as monaco from "monaco-editor";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+// import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+// import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+// import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+// import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 import { service } from "../dot-monaco/client.js";
 
@@ -22,17 +22,17 @@ langs.registerColorProvider("dot", service.colorProvider);
 self.MonacoEnvironment = {
 	getWorker(_: unknown, label: string) {
 		switch (label) {
-			case "json":
-				return new jsonWorker();
-			case "css":
-			case "scss":
-			case "less":
-				return new cssWorker();
-			case "html":
-				return new htmlWorker();
-			case "typescript":
-			case "javascript":
-				return new tsWorker();
+			// case "json":
+			// 	return new jsonWorker();
+			// case "css":
+			// case "scss":
+			// case "less":
+			// 	return new cssWorker();
+			// case "html":
+			// 	return new htmlWorker();
+			// case "typescript":
+			// case "javascript":
+			// 	return new tsWorker();
 			default:
 				return new editorWorker();
 		}
