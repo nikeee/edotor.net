abd6051a99821d085c988c09884142dec7bab1cd
Niklas Mollenhauer <nikeee@outlook.com>
2025-12-06 03:24:26 +0100

    Add demo component

---
+++ src/components/Editor.tsx
@@ -0,0 +1,40 @@
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api.js";
+import { useState } from "react";
+
+import "./importWorker";
+
+export default function Editor() {
+	const [editor, setEditor] =
+		useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: "100vh",
+			}}
+			ref={monacoEl => {
+				if (!monacoEl) {
+					return;
+				}
+
+				setEditor(editor => {
+					if (editor) {
+						return editor;
+					}
+
+					return monaco.editor.create(monacoEl, {
+						value: [
+							"function x() {",
+							'\tconsole.log("Hello world!");',
+							"}",
+						].join("\n"),
+						language: "typescript",
+					});
+				});
+
+				return () => editor?.dispose();
+			}}
+		></div>
+	);
+}
