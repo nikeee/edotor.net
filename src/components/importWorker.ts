a158b1ade00ad26a11402821460b26b10e6f313b
Niklas Mollenhauer <nikeee@outlook.com>
2025-12-08 03:10:52 +0100

    Import langs

--- src/components/importWorker.ts
+++ src/components/importWorker.ts
@@ -1,40 +1,21 @@
-import * as monaco from "monaco-editor";
+import { languages } from "monaco-editor";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-// import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-// import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-// import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-// import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 import { service } from "../dot-monaco/index.js";
 
-const langs = monaco.languages;
-langs.register(service.language);
-langs.setMonarchTokensProvider("dot", service.monarchTokens);
-langs.setLanguageConfiguration("dot", service.languageConfig);
-langs.registerCompletionItemProvider("dot", service.completionItemProvider);
-langs.registerHoverProvider("dot", service.hoverProvider);
-langs.registerDefinitionProvider("dot", service.definitionProvider);
-langs.registerReferenceProvider("dot", service.referenceProvider);
-langs.registerRenameProvider("dot", service.renameProvider);
-langs.registerCodeActionProvider("dot", service.codeActionProvider);
-langs.registerColorProvider("dot", service.colorProvider);
+languages.register(service.language);
+languages.setMonarchTokensProvider("dot", service.monarchTokens);
+languages.setLanguageConfiguration("dot", service.languageConfig);
+languages.registerCompletionItemProvider("dot", service.completionItemProvider);
+languages.registerHoverProvider("dot", service.hoverProvider);
+languages.registerDefinitionProvider("dot", service.definitionProvider);
+languages.registerReferenceProvider("dot", service.referenceProvider);
+languages.registerRenameProvider("dot", service.renameProvider);
+languages.registerCodeActionProvider("dot", service.codeActionProvider);
+languages.registerColorProvider("dot", service.colorProvider);
 
 self.MonacoEnvironment = {
-	getWorker(_: unknown, label: string) {
-		switch (label) {
-			// case "json":
-			// 	return new jsonWorker();
-			// case "css":
-			// case "scss":
-			// case "less":
-			// 	return new cssWorker();
-			// case "html":
-			// 	return new htmlWorker();
-			// case "typescript":
-			// case "javascript":
-			// 	return new tsWorker();
-			default:
-				return new editorWorker();
-		}
+	getWorker(_: unknown, _label: string) {
+		return new editorWorker();
 	},
 };
