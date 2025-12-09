ceb329141bfd184b570ae2266f739d2edae8c909
Niklas Mollenhauer <nikeee@outlook.com>
2025-12-09 21:03:54 +0100

    Remove ported function

--- src/components/EditorPane.tsx
+++ src/components/EditorPane.tsx
@@ -1,6 +1,5 @@
 import { loader, default as MonacoEditor } from "@monaco-editor/react";
 import * as monaco from "monaco-editor";
-import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import { Component } from "react";
 
 import { saveLastSource } from "../config.js";
@@ -8,30 +7,22 @@ import * as ls from "../dot-monaco/index.js";
 
 type Monaco = typeof monaco;
 
-self.MonacoEnvironment = {
-	getWorker(_, _label) {
-		return new editorWorker();
-	},
-};
 loader.config({ monaco });
 
-type Props = {
+type EditorPaneProps = {
 	defaultValue?: string;
 	onChangeValue(value: string): void;
 	onValueError(err: monaco.editor.IMarkerData[]): void;
 };
 
-// biome-ignore lint/suspicious/noExplicitAny: will be migrated
-type State = any;
-
 const SOURCE_SAVE_TIMEOUT = 5 * 1000; // 5 seconds
 
-export default class EditorPane extends Component<Props, State> {
+export default class EditorPane extends Component<EditorPaneProps, object> {
 	#processor: ls.LanguageProcessor | undefined;
 	#editor: monaco.editor.IStandaloneCodeEditor | undefined;
 	#autoSaveTimeout: ReturnType<typeof setTimeout> | undefined = undefined;
 
-	state: State = {};
+	state: object = {};
 
 	loadValue(value: string) {
 		const e = this.#editor;
@@ -44,37 +35,6 @@ export default class EditorPane extends Component<Props, State> {
 		ls.registerService(monaco, ls.service);
 		this.#processor = ls.service.processor;
 	};
-
-	#editorDidMount = (
-		editor: monaco.editor.IStandaloneCodeEditor,
-		monaco: Monaco,
-	): void => {
-		this.#editor = editor;
-		ls.registerCommands(editor);
-
-		// the first render() is called before the language is registered
-		// We need to refresh the model with the identical value, but a different language
-		this.#refreshModel(editor, monaco);
-
-		editor.focus();
-	};
-
-	#refreshModel(editor: monaco.editor.IStandaloneCodeEditor, monaco: Monaco) {
-		const oldModel = editor.getModel();
-		try {
-			import.meta.env.DEV && console.assert(!!oldModel);
-
-			const newModel = monaco.editor.createModel(
-				oldModel ? oldModel.getValue() : "",
-				"dot",
-				monaco.Uri.parse("inmemory://tmp.dot"),
-			);
-
-			editor.setModel(newModel);
-		} finally {
-			if (oldModel) oldModel.dispose();
-		}
-	}
 
 	#onChange = (
 		value: string | undefined,
@@ -122,21 +82,8 @@ export default class EditorPane extends Component<Props, State> {
 		const defaultValue = this.props.defaultValue || "";
 		return (
 			<MonacoEditor
-				language="dot"
 				defaultValue={defaultValue}
-				options={{
-					selectOnLineNumbers: true,
-					lineNumbers: "on",
-					wordWrap: "on",
-					roundedSelection: false,
-					scrollBeyondLastLine: false,
-					minimap: { enabled: false },
-					automaticLayout: true,
-					folding: true,
-					glyphMargin: true,
-				}}
 				onChange={this.#onChange}
-				onMount={this.#editorDidMount}
 				beforeMount={this.#editorWillMount}
 			/>
 		);
