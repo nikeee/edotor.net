2111d9ab1030fa9b1d29bb1712047ebc7cb7680d
Niklas Mollenhauer <nikeee@outlook.com>
2025-12-10 13:35:25 +0100

    Rename vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { viteStaticCopy } from "vite-plugin-static-copy"
-import react from "@vitejs/plugin-react"
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd());
